ff84bd0049e20a2763adf3beed4a20f61c8f6929
giadarol <giovanni.iadarola@cern.ch>
2024-02-17 13:23:18 +0100

    We have alos Br

--- 000_dev.py
+++ 000_dev.py
@@ -18,13 +18,13 @@ def ellipp(n, m):
     rj = elliprj(0, y, 1, 1 - n)
     return rf + rj * n / 3
 
-L = 10
+L = 4
 a = 0.3
 B0 = 1.5
 
 # r = np.linspace(-0.5 * a, 0.5 * a, 100)
-r = 0.
-z = np.linspace(-L*1.3, L*1.3, 100)
+z = np.linspace(-L*1.3, L*1.3, 1000)
+r = 0 * z +0.01
 
 u = 4 * a * r / (a + r)**2
 zeta_plus = z + L / 2
@@ -50,10 +50,28 @@ Bz_minus = B0 * zeta_minus / (4 * np.pi) * sqrt_minus * (
 
 Bz = Bz_plus - Bz_minus
 
+sqrt_r_plus = 0 * sqrt_plus
+sqrt_r_minus = 0 * sqrt_minus
+
+mask_r_nonzero = r > 1e-11
+sqrt_r_plus[mask_r_nonzero] = np.sqrt(a / (r[mask_r_nonzero] * m_plus[mask_r_nonzero]))
+sqrt_r_minus[mask_r_nonzero] = np.sqrt(a / (r[mask_r_nonzero] * m_minus[mask_r_nonzero]))
+
+Br_plus = B0 / np.pi * sqrt_r_plus * (EE(m_plus) - (1 - m_plus / 2) * KK(m_plus))
+Br_minus = B0 / np.pi * sqrt_r_minus * (EE(m_minus) - (1 - m_minus / 2) * KK(m_minus))
+
+Br = Br_plus - Br_minus
+
+
 import matplotlib.pyplot as plt
 plt.close('all')
 
-fig, ax = plt.subplots(1, 1)
+ax1 = plt.subplot(2, 1, 1)
 plt.plot(z, Bz)
+plt.ylabel('Bz [T]')
+
+ax2 = plt.subplot(2, 1, 2, sharex=ax1)
+plt.plot(z, Br)
+plt.ylabel('Br [T]')
 
 plt.show()
