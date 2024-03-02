c03ca040443fcac55c4092af686666b7f6da6225
giadarol <giovanni.iadarola@cern.ch>
2024-03-02 11:16:17 +0100

    Prepare solenoid slices

--- 004b_install_solenoid.py
+++ 004b_install_solenoid.py
@@ -1,5 +1,7 @@
 import xtrack as xt
 import numpy as np
+from scipy.constants import c as clight
+from scipy.constants import e as qe
 
 line = xt.Line.from_json('fccee_z_thick.json')
 line.cycle('ip.4', inplace=True)
@@ -14,6 +16,18 @@ bz_df = pd.read_csv(bz_data_file, sep='\s+', skiprows=1, names=['z', 'Bz'])
 
 s_sol_slices = np.linspace(-2.2, 2.2, 80)
 bz_sol_slices = np.interp(s_sol_slices, bz_df.z, bz_df.Bz)
+
+P0_J = line.particle_ref.p0c[0] * qe / clight
+brho = P0_J / qe / line.particle_ref.q0
+ks = 0.5 * (bz_sol_slices[:-1] + bz_sol_slices[1:]) / brho
+l_sol_slices = np.diff(s_sol_slices)
+s_sol_slices_entry = s_sol_slices[:-1]
+
+sol_slices = []
+for ii in range(len(s_sol_slices_entry)):
+    sol_slices.append(
+        xt.Solenoid(length=l_sol_slices[ii]))
+
 
 # plot
 import matplotlib.pyplot as plt
