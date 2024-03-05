ec9cc9d8e4dd5c45e02dadd4f303c6be067e0d94
giadarol <giovanni.iadarola@cern.ch>
2024-03-05 16:28:55 +0100

    First radiation plot

---
+++ 004c_radiation.py
@@ -0,0 +1,30 @@
+import xtrack as xt
+import numpy as np
+from scipy.constants import c as clight
+from scipy.constants import e as qe
+
+line = xt.Line.from_json('fccee_z_thick_with_sol_corrected.json')
+tw_no_rad = line.twiss(method='4d')
+line.configure_radiation(model='mean')
+tt = line.get_table()
+ttmult = tt.rows[tt.element_type == 'Multipole']
+
+# RF on
+line.vars['voltca1'] = 13.2
+
+tw = line.twiss()
+
+eloss = np.diff(tw.ptau) * line.particle_ref.energy0[0]
+ds = np.diff(tt.s)
+
+mask_ds = ds > 0
+
+dE_ds = eloss * 0
+dE_ds[mask_ds] = -eloss[mask_ds] / ds[mask_ds]
+
+import matplotlib.pyplot as plt
+plt.close('all')
+plt.figure(1)
+plt.plot(tw.s[:-1], dE_ds, label='dE/ds')
+
+plt.show()
