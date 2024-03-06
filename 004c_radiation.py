723e93d9d22cd87eb23bc5565a6dda8a504a767b
giadarol <giovanni.iadarola@cern.ch>
2024-03-06 14:52:53 +0100

    Remove artefact at the beginning

--- 004c_radiation.py
+++ 004c_radiation.py
@@ -6,16 +6,17 @@ from scipy.constants import e as qe
 line = xt.Line.from_json('fccee_z_thick_with_sol_corrected.json')
 tw_no_rad = line.twiss(method='4d')
 line.configure_radiation(model='mean')
-tt = line.get_table()
+tt = line.get_table(attr=True)
 ttmult = tt.rows[tt.element_type == 'Multipole']
 
 # RF on
 line.vars['voltca1'] = 13.2
 
+
 tw = line.twiss()
 
 eloss = np.diff(tw.ptau) * line.particle_ref.energy0[0]
-ds = np.diff(tt.s)
+ds = tt.length[:-1]
 
 mask_ds = ds > 0
 
@@ -25,8 +26,15 @@ dE_ds[mask_ds] = -eloss[mask_ds] / ds[mask_ds]
 import matplotlib.pyplot as plt
 plt.close('all')
 plt.figure(1)
-plt.plot(tw.s[:-1], dE_ds * 1e-2 * 1e-3, label='dE/ds')
+ax1 = plt.subplot(3, 1, 1)
+plt.plot(tw.s[:-1], dE_ds * 1e-2 * 1e-3, '.-', label='dE/ds')
 plt.xlabel('s [m]')
 plt.ylabel('dE/ds [keV/m]')
+ax2 = plt.subplot(3, 1, 2, sharex=ax1)
+plt.plot(tw.s, tt.ks, '.-', label='ks')
+plt.xlabel('s [m]')
+ax3 = plt.subplot(3, 1, 3, sharex=ax1)
+plt.plot(tw.s[:-1], np.diff(tt.ks), '.-', label='ks')
+plt.xlabel('s [m]')
 
 plt.show()
